9b41ef3da8f5c206c466988fb4daca88a17141f0
Rajeshpaiks77 <paikrao.rajesh8390@gmail.com>
2025-07-27 01:24:05 +0530

    Revert "Revert "RC""

---
+++ my_new_stack/my_new_cdk_stack.py
@@ -0,0 +1,19 @@
+from aws_cdk import (
+    # Duration,
+    Stack,
+    # aws_sqs as sqs,
+)
+from constructs import Construct
+
+class MyNewCdkStack(Stack):
+
+    def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
+        super().__init__(scope, construct_id, **kwargs)
+
+        # The code that defines your stack goes here
+
+        bucket = Bucket(self, "raj_first_bucket",
+            versioned=True,  # Enable versioning
+            removal_policy=RemovalPolicy.DESTROY,  # Remove bucket when stack is destroyed
+            auto_delete_objects=True  # Auto-delete objects with bucket (only works with DESTROY)
+        )
