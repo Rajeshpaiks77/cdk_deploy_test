993f1461d2eacb2cee4771c0ad580a5fa94cbd37
Rajesh <paikrao.rajesh8390@gmail.com>
2025-07-27 00:54:56 +0530

    RC

---
+++ my_new_stack/my_new_cdk_stack.py
@@ -0,0 +1,14 @@
+from aws_cdk import (
+    # Duration,
+    Stack,
+    aws_s3 as s3
+)
+from constructs import Construct
+
+class MyNewCdkStack(Stack):
+
+    def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
+        super().__init__(scope, construct_id, **kwargs)
+        
+        s3.Bucket(self,id="RjBucket",bucket_name="RJ_BUCKET")
+    
