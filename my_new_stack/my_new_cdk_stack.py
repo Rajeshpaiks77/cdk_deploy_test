788ae2d9583722113c20762e4dc33183bf143a07
Rajesh <paikrao.rajesh8390@gmail.com>
2025-07-27 04:25:37 +0530

    RC

--- my_new_stack/my_new_cdk_stack.py
+++ my_new_stack/my_new_cdk_stack.py
@@ -15,8 +15,4 @@ class MyNewCdkStack(Stack):
 
         # The code that defines your stack goes here
 
-        bucket = s3.Bucket(self, "raj_first_bucket",
-            versioned=True,  # Enable versioning
-            removal_policy=RemovalPolicy.DESTROY,  # Remove bucket when stack is destroyed
-            auto_delete_objects=True  # Auto-delete objects with bucket (only works with DESTROY)
-        )
+
