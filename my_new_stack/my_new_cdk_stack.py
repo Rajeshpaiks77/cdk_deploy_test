313a7541bb539333416d8c0de6cbb499c8de4c5f
Rajesh <paikrao.rajesh8390@gmail.com>
2025-07-27 02:56:27 +0530

    RC

--- my_new_stack/my_new_cdk_stack.py
+++ my_new_stack/my_new_cdk_stack.py
@@ -1,9 +1,11 @@
 from aws_cdk import (
     # Duration,
     Stack,
-    aws_s3 as Bucket,
-    RemovalPolicy
+    RemovalPolicy,
+    aws_s3 as s3
+
 )
+from aws_cdk import Stack, RemovalPolicy
 from constructs import Construct
 
 class MyNewCdkStack(Stack):
@@ -13,7 +15,7 @@ class MyNewCdkStack(Stack):
 
         # The code that defines your stack goes here
 
-        bucket = Bucket(self, "raj_first_bucket",
+        bucket = s3.Bucket(self, "raj_first_bucket",
             versioned=True,  # Enable versioning
             removal_policy=RemovalPolicy.DESTROY,  # Remove bucket when stack is destroyed
             auto_delete_objects=True  # Auto-delete objects with bucket (only works with DESTROY)
